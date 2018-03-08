653d262ef1d0bfed9b271a576d7725be189fd971
Rohan Taori <rtaori13@gmail.com>
2018-03-08 12:55:52 -0800

    monitoring deterministic values

--- baselines/acktr/plot_vf.py
+++ baselines/acktr/plot_vf.py
@@ -4,16 +4,30 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-vf_of_base_obs = np.load('value_functions.npy')
-vf_of_base_obs = np.rollaxis(vf_of_base_obs, axis=0, start=3)
+# vf_of_base_obs = np.load('value_functions.npy')
+# vf_of_base_obs = np.rollaxis(vf_of_base_obs, axis=0, start=3)
 
-print(vf_of_base_obs.shape)
+# print(vf_of_base_obs.shape)
 
-for i in range(len(vf_of_base_obs)):
-	for j in range(len(vf_of_base_obs[i])):
-		plt.plot(vf_of_base_obs[i][j])
-		plt.title('Value of State {} in Path {} Over Time'.format(j, i))
-		plt.xlabel('Number of Timesteps')
-		plt.ylabel('Expected Future Reward')
-		plt.savefig('plots/state{}_path{}.png'.format(j, i))
-		plt.close()
+# for i in range(len(vf_of_base_obs)):
+# 	for j in range(len(vf_of_base_obs[i])):
+# 		plt.plot(vf_of_base_obs[i][j])
+# 		plt.title('Value of State {} in Path {} Over Time'.format(j, i))
+# 		plt.xlabel('Number of Timesteps')
+# 		plt.ylabel('Expected Future Reward')
+# 		plt.savefig('plots/state{}_path{}.png'.format(j, i))
+# 		plt.close()
+
+
+value_data = np.load('plots/reacher-plots-2/value_estimates.npy')
+for init_state in range(value_data.shape[0]):
+	plt.plot(range(2500, 662500+1, 7500), value_data[init_state][0], label='avg rollout rewards for init state', c='blue')
+	plt.plot(range(2500, 662500+1, 7500), value_data[init_state][2], label='estimated rewards for init state', c='blue', linestyle='dashed')
+	plt.plot(range(2500, 662500+1, 7500), value_data[init_state][1], label='avg rollout rewards for state after init', c='red')
+	plt.plot(range(2500, 662500+1, 7500), value_data[init_state][3], label='estimated rewards for state after init', c='red', linestyle='dashed')
+	plt.title('Reward estimation for state {}'.format(init_state))
+	plt.xlabel('Number of iterations')
+	plt.ylabel('Reward')
+	plt.legend()
+	plt.savefig('plots/reacher-plots-2/state{}.png'.format(init_state))
+	plt.close()
