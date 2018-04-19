f26c8d2d433ac9c2f3427be68f316aba4145027a
Rohan Taori <rtaori13@gmail.com>
2018-04-19 12:46:40 -0700

    add prioritized dci

--- baselines/acktr/value_functions_nonparametric.py
+++ baselines/acktr/value_functions_nonparametric.py
@@ -1,8 +1,8 @@
 from baselines import logger
 import baselines.common as common
 import numpy as np
-from sklearn.neighbors import KDTree
 from sklearn.externals import joblib
+from dci import DCI
 import tensorflow as tf
 import time
 
@@ -11,9 +11,19 @@ class LinearDensityValueFunction(object):
     def __init__(self, n_neighbors, ob_dim, ac_dim):
         self.n_neighbors = n_neighbors
         self.dim = 2 * ob_dim + 2 * ac_dim + 1
-        self.tree = KDTree([[None]])
         self.X, self.y = None, None
         self.timestep_to_numdatapoints_dict = {}
+
+        # prioritized DCI
+        self.num_comp_indices = 2
+        self.num_simp_indices = 7
+        self.num_levels = 2
+        self.construction_field_of_view = 10
+        self.construction_prop_to_retrieve = 0.002
+        self.query_field_of_view = 100
+        self.query_prop_to_retrieve = 0.05
+
+        self.dci = DCI(self.dim, self.num_comp_indices, self.num_simp_indices)
 
         # define the graph for linear regression
         self.X_linreg = tf.placeholder(tf.float32, shape=[None, self.n_neighbors, self.dim])
@@ -41,8 +51,8 @@ class LinearDensityValueFunction(object):
         return self._predict_linreg(X_query)
 
     def _predict_linreg(self, X_query):
-        ind = self.tree.query(X_query, k=self.n_neighbors, return_distance=False)
-        X, y = self.X[ind], self.y[ind]
+        nn_idx, _ = dci_db.query(X_query, num_neighbours=self.n_neighbors, field_of_view=self.query_field_of_view, prop_to_retrieve=self.query_prop_to_retrieve, blind=True)
+        X, y = self.X[nn_idx], self.y[nn_idx]
         # X_t = X.swapaxes(1, 2)
         # inv = np.linalg.inv(X_t @ X + 1e-2 * np.eye(X.shape[2]))
         # end = np.einsum('ijk,ik->ij', X_t, y)
@@ -66,14 +76,16 @@ class LinearDensityValueFunction(object):
             self.X = np.vstack((self.X, X))
             self.y = np.hstack((self.y, y))
 
-        self.tree = KDTree(self.X, leaf_size=10)
+        self.dci.add(self.X, num_levels=self.num_levels, 
+            field_of_view=self.construction_field_of_view, prop_to_retrieve=self.construction_prop_to_retrieve)
+
         self.timestep_to_numdatapoints_dict[timesteps_so_far] = self.X.shape[0]
 
         if self.is_fit():
             logger.record_tabular("EVAfter", common.explained_variance(self._predict_linreg(X), y))
 
     def is_fit(self):
-        return self.tree.data.shape[0] > self.n_neighbors
+        return self.X.shape[0] > self.n_neighbors
 
     def save_model(self, data_filename, dict_filename):
         joblib.dump(self.X, data_filename)
